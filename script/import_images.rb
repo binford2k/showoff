b2a72b2aed6e509e2dbfe839dc41b9e1be340e3a
Scott Chacon <schacon@gmail.com>
2010-01-11 15:12:41 -0800

    added script to add a bunch of images as a section

---
+++ script/import_images.rb
@@ -0,0 +1,45 @@
+#! /usr/bin/env ruby
+
+# this script will take a directory full of images and make a showoff
+# presentation section out of them
+
+# usage: ./import_images.rb preso section_name /path/to/images
+
+require 'fileutils'
+require 'pp'
+
+if ARGV.size < 3
+  puts 'usage: ./import_images.rb preso section_name /path/to/images'
+  exit
+end
+
+preso = ARGV[0]
+section = ARGV[1]
+path = ARGV[2]
+
+# look for showoff file
+if File.exists?(preso)
+  Dir.chdir(preso) do
+    # make the new directory
+    Dir.mkdir(section)
+    Dir.chdir(section) do
+      Dir.mkdir('img')
+      # copy all the images into img dir
+      FileUtils.cp_r(path, "img")
+      files = Dir.glob("img/**/*")
+
+      # create the slides file
+      filenm = "#{section}.md"
+      File.open(filenm, 'w+') do |f|
+        files.each do |img|
+          if File.file?(img)
+            f.puts "!SLIDE center"
+            f.puts "![#{img}](#{img})"
+            f.puts
+          end
+        end
+      end
+      pp files
+    end
+  end
+end
