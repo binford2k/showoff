435cc6f15f6e2b0caeb58de556fa7ca1f76df696
Yossef Mendelssohn <ymendel@pobox.com>
2012-05-26 12:22:20 -0400

    Refactoring "special content" test.
    Putting in some useful helper methods and variables, making the tests
    all look the same.

--- test/special_content_test.rb
+++ test/special_content_test.rb
@@ -13,38 +13,58 @@ context "ShowOff Special Content tests" do
     html.css('div.slide')[i-1]
   end
 
+  def get_notes(slide)
+    slide.css('p.notes')
+  end
+
+  def get_notes_contents(slide)
+    get_notes(slide).text
+  end
+
   test 'converts `.notes` lines to notes-class paragraphs' do
     slide = get_slide(1)
-    assert_equal 'Some notes for the first slide', slide.css('p.notes').text
+    expected = 'Some notes for the first slide'
+
+    assert_equal expected, get_notes_contents(slide)
   end
 
   test 'handles notes lines placed before slide content' do
     slide = get_slide(2)
-    assert_equal 'Some more notes, longer and more interesting I guess', slide.css('p.notes').text
+    expected = 'Some more notes, longer and more interesting I guess'
+
+    assert_equal expected, get_notes_contents(slide)
   end
 
   test 'handles notes split across lines' do
     slide = get_slide(3)
-    assert_equal "Sometimes notes can go on and on and on and on and on\nand on and on, much longer than you need them to.", slide.css('p.notes').text
+    expected = "Sometimes notes can go on and on and on and on and on\nand on and on, much longer than you need them to."
+
+    assert_equal expected, get_notes_contents(slide)
   end
   
   test 'handles multi-line notes with the special-content marker repeated' do
     slide = get_slide(4)
-    assert_equal "Sometimes notes really do go on so long\nyou just have no idea what to do with them\nand you don't want to keep on saying .notes\nbut you can't find a way to make yourself stop.", slide.css('p.notes').text
+    expected = "Sometimes notes really do go on so long\nyou just have no idea what to do with them\nand you don't want to keep on saying .notes\nbut you can't find a way to make yourself stop."
+
+    assert_equal expected, get_notes_contents(slide)
   end
 
   test 'handles slides without notes' do
     slide = get_slide(5)
-    assert_equal 0, slide.css('p.notes').length
+    assert_equal 0, get_notes(slide).length
   end
 
   test 'handles slides with empty notes' do
     slide = get_slide(6)
-    assert_equal '', slide.css('p.notes').text
+    expected = ''
+
+    assert_equal expected, get_notes_contents(slide)
   end
   
   test 'handles multi-line notes with the special-content marker repeated' do
     slide = get_slide(7)
-    assert_equal "Sometimes notes have something interesting to say.\n\nAnd you just want to pause and think about it.\n\n  And think about it some more.", slide.css('p.notes').text
+    expected = "Sometimes notes have something interesting to say.\n\nAnd you just want to pause and think about it.\n\n  And think about it some more."
+
+    assert_equal expected, get_notes_contents(slide)
   end
 end
