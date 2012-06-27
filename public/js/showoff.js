6fa4971a121778a04f06604c4e0a1af82946b253
Ralph Luchs <ralph@puppetlabs.com>
2012-06-26 21:46:32 -0600

    preshow text typo

--- public/js/showoff.js
+++ public/js/showoff.js
@@ -667,7 +667,7 @@ function startPreShow() {
 
 function addPreShowTips() {
 	time = secondsToTime(preshow_secondsLeft)
-	$('#preshow_timer').text('Class will resume in' + time)
+	$('#preshow_timer').text('Class will resume in ' + time)
 	var des = preshow_des && preshow_des[tmpImg.attr("ref")]
 	if(des) {
 		$('#tips').show()
