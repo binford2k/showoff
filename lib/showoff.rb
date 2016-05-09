ace3f8e52b0d699befe0543eefb2b0fec318fca7
Ben Ford <ben.ford@puppetlabs.com>
2016-05-08 19:56:15 -0700

    Fixes broken follow mode

--- lib/showoff.rb
+++ lib/showoff.rb
@@ -1362,7 +1362,7 @@ class ShowOff < Sinatra::Application
     else
       request.websocket do |ws|
         ws.onopen do
-          ws.send( { 'current' => @@current[:number] }.to_json )
+          ws.send( { 'message' => 'current', 'current' => @@current[:number] }.to_json )
           settings.sockets << ws
 
           @logger.warn "Open sockets: #{settings.sockets.size}"
@@ -1392,7 +1392,7 @@ class ShowOff < Sinatra::Application
                 @@current = { :name => name, :number => slide }
 
                 # schedule a notification for all clients
-                EM.next_tick { settings.sockets.each{|s| s.send({ 'current' => @@current[:number] }.to_json) } }
+                EM.next_tick { settings.sockets.each{|s| s.send({ 'message' => 'current', 'current' => @@current[:number] }.to_json) } }
               end
 
             when 'register'
