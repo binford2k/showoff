8933570c208c2d632ec7e86a1baab531edc9944d
Ben Ford <ben.ford@puppetlabs.com>
2018-01-18 00:21:43 -0800

    (maint) Clear presenter cookie too.

--- public/js/showoff.js
+++ public/js/showoff.js
@@ -615,6 +615,7 @@ function clearCookies() {
   document.cookie = "layout=;expires=Thu, 21 Sep 1979 00:00:01 UTC;";
   document.cookie = "notes=;expires=Thu, 21 Sep 1979 00:00:01 UTC;";
   document.cookie = "tourVersion=;expires=Thu, 21 Sep 1979 00:00:01 UTC;";
+  document.cookie = "presenter=;expires=Thu, 21 Sep 1979 00:00:01 UTC;";
   document.cookieHash = {};
 }
 
