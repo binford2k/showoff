c87848474994d5dea9fbc4bae15c7993aeaa00d5
Ben Ford <ben.ford@puppetlabs.com>
2018-01-25 21:00:00 -0800

    translate nodes individually
    Instead of just doing a simple string replacement, this will identify
    text nodes and other DOM nodes contained in an element. It will iterate
    each and translate them individually. Text nodes are interpolated
    directly, and DOM nodes are recursed on until the text they contain is
    interpolated.
    
    Supersedes #848

--- public/js/simpleStrings-0.0.1.js
+++ public/js/simpleStrings-0.0.1.js
@@ -29,10 +29,7 @@
         strings: {}
       }, options );
 
-      function translate(item) {
-        item = $(item);
-        var text = item.text();
-
+      function interpolate(text) {
         var re = /{{([^}]+)}}/;
         while(m = re.exec(text)) {
           var keyword = m[1];
@@ -40,9 +37,23 @@
             text = text.replace('{{'+keyword+'}}', settings.strings[keyword]);
           }
         }
-        item.text(text);
 
-        return item;
+        return text;
+      }
+
+      function translate(item) {
+        // iterate through nodes contained in this element, including plain text nodes and other elements
+        return $(item).contents()
+          .each(function() {
+            if(this.nodeType == 3) {
+              // translate directly if it's text
+              this.replaceWith(interpolate(this.textContent));
+            }
+            else {
+              // otherwise recurse inside and try again
+              translate(this);
+            }
+          });
       }
 
       function inline_svg(img, callback) {
