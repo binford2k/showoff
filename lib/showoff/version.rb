697d1540c80ddfbf0a30a7639aaa43b17d77580b
Ben Ford <ben.ford@puppetlabs.com>
2021-04-11 23:39:21 -0700

    Releasing v0.20.3

--- lib/showoff/version.rb
+++ lib/showoff/version.rb
@@ -1,3 +1,3 @@
 # No namespace here since Showoff is a class and I'd have to inherit from
 # Sinatra::Application (which we don't want to load here)
-SHOWOFF_VERSION = '0.20.2'
+SHOWOFF_VERSION = '0.20.3'
